1e0730ab30d4b765ea54646d106c53d85821bae5
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2016-01-18 15:25:09 +0200

    remove ErrorActionPreference from modules
    This should be set by application.

--- JujuHelper/JujuHelper.psm1
+++ JujuHelper/JujuHelper.psm1
@@ -12,8 +12,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-$ErrorActionPreference = "Stop"
-
 $version = $PSVersionTable.PSVersion.Major
 if ($version -lt 4){
     # Get-CimInstance is not supported on powershell versions earlier then 4
--- Networking/Networking.psm1
+++ Networking/Networking.psm1
@@ -12,8 +12,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-$ErrorActionPreference = "Stop"
-
 function Invoke-DHCPRenew {
     [CmdletBinding()]
     Param(
