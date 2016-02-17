fca984179a8a797b9a71f96cb83349ca6179dc09
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2016-02-17 14:41:12 +0200

    use GetStreamAsync in Invoke-FastWebRequest

--- JujuHelper/JujuHelper.psm1
+++ JujuHelper/JujuHelper.psm1
@@ -138,30 +138,19 @@ function Invoke-FastWebRequest {
         }
 
         $client = new-object System.Net.Http.HttpClient
-        $task = $client.GetAsync($Uri)
-        $task.wait()
+        $task = $client.GetStreamAsync($Uri)
+        if($task.IsFaulted) {
+            Throw $task.Exception
+        }
         $response = $task.Result
-        $status = $response.EnsureSuccessStatusCode()
-
         $outStream = New-Object IO.FileStream $OutFile, Create, Write, None
 
         try {
-            $task = $response.Content.ReadAsStreamAsync()
-            $task.Wait()
-            $inStream = $task.Result
-
-            $contentLength = $response.Content.Headers.ContentLength
-
             $totRead = 0
             $buffer = New-Object Byte[] 1MB
-            while (($read = $inStream.Read($buffer, 0, $buffer.Length)) -gt 0) {
+            while (($read = $response.Read($buffer, 0, $buffer.Length)) -gt 0) {
                 $totRead += $read
                 $outStream.Write($buffer, 0, $read);
-
-                if($contentLength){
-                    $percComplete = $totRead * 100 / $contentLength
-                    Write-Progress -Activity "Downloading: $Uri" -PercentComplete $percComplete
-                }
             }
         }
         finally {
